4f7cf92f46fd54fc71b5c3bee15db74e6e1e3c71
shadow <shadow>
2001-03-15 17:56:41 +0000

    DELTA fix-for-cache-consistency-on-linux-24-smp-systems-20010315 AUTHOR ejburcka@us.ibm.com "Here are the symptoms.  I have 2 OpenAFS  Linux 2.4 machines, one SMP and one not. --With both of them I access a series of directories and files within (/afs/w/x/y/z/). --I then move to the root directory. --From a third machine, I remove the directories x, y, and z. Then re-add them (same names) with some other files. --From the SMP box, I do an ls in /afs/w and see x.  But x is inaccessible. --From the UP box I can see and access x. "

--- src/afs/LINUX/osi_vnodeops.c
+++ src/afs/LINUX/osi_vnodeops.c
@@ -628,6 +628,13 @@ static int afs_linux_revalidate(struct dentry *dp)
 	return 0;
     }
 
+    /* Drop the dentry if the callback is broken */
+    if (!(vcp->states & CStatd)) {
+        d_drop(dp); 
+        AFS_GUNLOCK();
+        return 0;
+    }
+
     /* Make this a fast path (no crref), since it's called so often. */
     if (vcp->states & CStatd) {
         if (*dp->d_name.name != '/' && vcp->mvstat == 2) /* root vnode */
