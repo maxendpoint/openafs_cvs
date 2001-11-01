d5c4e71a3a1e42382f2580ad90aa5e4827a7aa76
shadow <shadow>
2001-11-01 05:18:30 +0000

    DELTA kdump-display-timeout-for-cell-info-20011101 AUTHOR kolya@mit.edu
    added field in kdump output to display afsdb timeout info
    DELTA some-name-yyyymmdd
    AUTHOR contributor@some.site

--- src/venus/kdump.c
+++ src/venus/kdump.c
@@ -10,7 +10,7 @@
 #include <afsconfig.h>
 #include <afs/param.h>
 
-RCSID("$Header: /cvs/openafs/src/venus/kdump.c,v 1.16 2001/11/01 04:02:31 shadow Exp $");
+RCSID("$Header: /cvs/openafs/src/venus/kdump.c,v 1.17 2001/11/01 05:18:30 shadow Exp $");
 
 #include <stdio.h>
 #include <stdlib.h>	/* for malloc() */
@@ -2259,9 +2259,9 @@ void print_cell(kmem, clep, ptr, pnt)
     cellName[40] = 0;
     Sum_cellnames += strlen(cellName)+1;
     if (pnt) {
-	printf("%lx: cell=%s, cellname=%s, states=%x, cindex=%d fsport=%d vlport=%d\n", ptr,
+	printf("%lx: cell=%s, cellname=%s, states=%x, cindex=%d fsport=%d vlport=%d timeout=%d\n", ptr,
 	       PrintIPAddr(clep->cell), cellName, clep->states, clep->cellIndex,
-	       clep->fsport, clep->vlport);
+	       clep->fsport, clep->vlport, clep->timeout);
 #ifdef	AFS33
 	if (clep->lcellp)
 	    printf("\tlinked cellp %lx\n", clep->lcellp);
