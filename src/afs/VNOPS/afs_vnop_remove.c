52877cb52ab66cb7aa3ad402b147d56a6b438f0c
kolya <kolya>
2002-03-24 19:08:45 +0000

    DELTA initialize-tvc-on-osf-20020324 AUTHOR kolya@mit.edu
    Initialize tvc in afs_remove so that we don't call afs_PutVCache
    on stack garbage in case of some failure.

--- src/afs/VNOPS/afs_vnop_remove.c
+++ src/afs/VNOPS/afs_vnop_remove.c
@@ -22,7 +22,7 @@
 #include <afsconfig.h>
 #include "../afs/param.h"
 
-RCSID("$Header: /cvs/openafs/src/afs/VNOPS/afs_vnop_remove.c,v 1.11 2002/03/20 18:38:28 shadow Exp $");
+RCSID("$Header: /cvs/openafs/src/afs/VNOPS/afs_vnop_remove.c,v 1.12 2002/03/24 19:08:45 kolya Exp $");
 
 #include "../afs/sysincludes.h"	/* Standard vendor system headers */
 #include "../afs/afsincludes.h"	/* Afs-based standard headers */
@@ -236,6 +236,10 @@ afs_remove(OSI_VC_ARG(adp), aname, acred)
     AFS_STATCNT(afs_remove);
     afs_Trace2(afs_iclSetp, CM_TRACE_REMOVE, ICL_TYPE_POINTER, adp,
 	       ICL_TYPE_STRING, aname);
+
+#ifdef	AFS_OSF_ENV
+    tvc = (struct vcache *)ndp->ni_vp;  /* should never be null */
+#endif
 
     /* Check if this is dynroot */
     if (afs_IsDynroot(adp)) {
