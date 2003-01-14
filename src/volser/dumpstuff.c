9a0ef1fe827a80c8317312d2f6d8961dffeb81a5
kolya <kolya>
2003-01-14 01:05:11 +0000

    DELTA better-assert-macro-20030113 AUTHOR kolya@mit.edu
    Wrap our assert() macro in do{..}while(0) to make it look
    just like a real function.  Fix instances of use of this
    macro that lacked a trailing semicolon of their own.

--- src/volser/dumpstuff.c
+++ src/volser/dumpstuff.c
@@ -10,7 +10,7 @@
 #include <afsconfig.h>
 #include <afs/param.h>
 
-RCSID("$Header: /cvs/openafs/src/volser/dumpstuff.c,v 1.9 2003/01/07 23:38:32 shadow Exp $");
+RCSID("$Header: /cvs/openafs/src/volser/dumpstuff.c,v 1.10 2003/01/14 01:05:11 kolya Exp $");
 
 #include <sys/types.h>
 #include <ctype.h>
@@ -677,8 +677,8 @@ static int DumpVnodeIndex(register struct iod *iodp, Volume *vp,
     assert(size != -1);
     nVnodes = (size / vcp->diskSize) - 1;
     if (nVnodes > 0) {
-	assert((nVnodes+1)*vcp->diskSize == size)
-	assert(STREAM_SEEK(file, vcp->diskSize, 0) == 0)
+	assert((nVnodes+1)*vcp->diskSize == size);
+	assert(STREAM_SEEK(file, vcp->diskSize, 0) == 0);
     }
     else nVnodes = 0;
     for (vnodeIndex = 0; nVnodes && STREAM_READ(vnode, vcp->diskSize, 1, file) == 1 && !code;
@@ -818,7 +818,7 @@ int ProcessIndex(Volume *vp, VnodeClass class, afs_int32 **Bufp, int *sizep,
 	OS_SYNC(afile->str_fd);
     } else {
 	size = OS_SIZE(fdP->fd_fd);
-	assert(size != -1)
+	assert(size != -1);
 	nVnodes = (size <= vcp->diskSize ? 0 :
 		   size-vcp->diskSize) >> vcp->logSize;
 	if (nVnodes > 0) {
