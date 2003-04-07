18daad2a8e6649c75410f8e31649acd622b03b83
shadow <shadow>
2003-04-07 17:38:56 +0000

    DELTA flushhostcps-20030407 AUTHOR shadow@dementia.org FIXES
    make it so flushhostcps can't try to Release when no host was found,
    which was apparently the problem dlc@cs really observed.

--- src/viced/host.c
+++ src/viced/host.c
@@ -10,7 +10,7 @@
 #include <afsconfig.h>
 #include <afs/param.h>
 
-RCSID("$Header: /cvs/openafs/src/viced/host.c,v 1.44 2003/04/02 00:22:16 shadow Exp $");
+RCSID("$Header: /cvs/openafs/src/viced/host.c,v 1.45 2003/04/07 17:38:56 shadow Exp $");
 
 #include <stdio.h>
 #include <errno.h>
@@ -504,10 +504,10 @@ void h_flushhostcps(register afs_uint32 hostaddr, register afs_uint32 hport)
     H_LOCK
     host = h_Lookup_r(hostaddr, hport, &held);
     if (host) {
-      host->hcpsfailed = 1;
+	host->hcpsfailed = 1;
+	if (!held)
+	    h_Release_r(host);
     }
-    if (!held)
-      h_Release_r(host);
     H_UNLOCK
 
     return;
