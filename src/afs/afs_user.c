0ceba131ce99c996e8134d5f008ecdf934260f53
shadow <shadow>
2003-05-08 18:53:48 +0000

    DELTA linux-gcpags-deal-correctly-with-uid-tokens-20030508 AUTHOR shadow@dementia.org FIXES
    look at the correct uid, so we don't gc in use tokens

--- src/afs/afs_user.c
+++ src/afs/afs_user.c
@@ -13,7 +13,7 @@
 #include <afsconfig.h>
 #include "afs/param.h"
 
-RCSID("$Header: /cvs/openafs/src/afs/afs_user.c,v 1.12 2002/10/16 03:58:16 shadow Exp $");
+RCSID("$Header: /cvs/openafs/src/afs/afs_user.c,v 1.13 2003/05/08 18:53:48 shadow Exp $");
 
 #include "afs/stds.h"
 #include "afs/sysincludes.h"	/* Standard vendor system headers */
@@ -587,7 +587,7 @@ void afs_GCPAGs_perproc_func(AFS_PROC *pproc)
     afs_GCPAGs_cred_count++;
 
     pag = PagInCred(pcred);
-#if defined(AFS_DARWIN_ENV) || defined(AFS_FBSD40_ENV)
+#if defined(AFS_DARWIN_ENV) || defined(AFS_FBSD40_ENV) || defined(AFS_LINUX22_ENV)
     uid = (pag != NOPAG ? pag : pcred->cr_uid);
 #else
     uid = (pag != NOPAG ? pag : pcred->cr_ruid);
