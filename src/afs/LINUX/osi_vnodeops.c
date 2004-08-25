ab7c895b336cbec15cc9f4ebbcbc3c7b149e8c3e
shadow <shadow>
2004-08-25 07:04:05 +0000

    DELTA STABLE14-initialize-i-security-20040816 AUTHOR aedil@alchar.org FIXES
    modern 2.6 kernels with i_security need this such that if the i_security field got garbage-collected out from under us we don't deref a null pointer.

--- src/afs/LINUX/osi_vnodeops.c
+++ src/afs/LINUX/osi_vnodeops.c
@@ -22,7 +22,7 @@
 #include "afs/param.h"
 
 RCSID
-    ("$Header: /cvs/openafs/src/afs/LINUX/osi_vnodeops.c,v 1.81 2004/07/21 22:23:38 shadow Exp $");
+    ("$Header: /cvs/openafs/src/afs/LINUX/osi_vnodeops.c,v 1.81.2.1 2004/08/25 07:04:05 shadow Exp $");
 
 #include "afs/sysincludes.h"
 #include "afsincludes.h"
@@ -1085,6 +1085,12 @@ afs_linux_lookup(struct inode *dip, struct dentry *dp)
 	    printk
 		("afs_linux_lookup: ip->i_mode 0x%x  dp->d_name.name %s  code %d\n",
 		 ip->i_mode, dp->d_name.name, code);
+#ifdef STRUCT_INODE_HAS_I_SECURITY
+       if (ip->i_security == NULL) {
+           if (security_inode_alloc(ip))
+               panic("afs_linux_lookup: Cannot allocate inode security");
+       }
+#endif
 #else
 	if (S_ISDIR(ip->i_mode))
 	    ip->i_op = &afs_dir_iops;
