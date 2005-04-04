0553b568e7cda4014e6b3ba116e7f2d96594398e
shadow <shadow>
2005-04-04 03:57:04 +0000

    DELTA macos103-update-20050403 AUTHOR shadow@dementia.org FIXES
    use system vnodes. one remaining bug, dangling vnodes at shutdown.

--- src/afs/VNOPS/afs_vnop_attrs.c
+++ src/afs/VNOPS/afs_vnop_attrs.c
@@ -24,7 +24,7 @@
 #include "afs/param.h"
 
 RCSID
-    ("$Header: /cvs/openafs/src/afs/VNOPS/afs_vnop_attrs.c,v 1.33 2005/04/03 20:40:39 shadow Exp $");
+    ("$Header: /cvs/openafs/src/afs/VNOPS/afs_vnop_attrs.c,v 1.34 2005/04/04 03:57:04 shadow Exp $");
 
 #include "afs/sysincludes.h"	/* Standard vendor system headers */
 #include "afsincludes.h"	/* Afs-based standard headers */
@@ -93,16 +93,10 @@ afs_CopyOutAttrs(register struct vcache *avc, register struct vattr *attrs)
     attrs->va_fsid = avc->v.v_vfsp->vfs_fsid.val[0];
 #elif defined(AFS_OSF_ENV)
     attrs->va_fsid = avc->v.v_mount->m_stat.f_fsid.val[0];
-<<<<<<< afs_vnop_attrs.c
 #else
 #ifdef AFS_DARWIN70_ENV
     attrs->va_fsid = avc->v->v_mount->mnt_stat.f_fsid.val[0];
 #else /* ! AFS_DARWIN70_ENV */
-=======
-#elif defined(AFS_DARWIN70_ENV)
-    attrs->va_fsid = avc->v.v_mount->mnt_stat.f_fsid.val[0];
-#else 
->>>>>>> 1.32
     attrs->va_fsid = 1;
 #endif 
     if (avc->mvstat == 2) {
