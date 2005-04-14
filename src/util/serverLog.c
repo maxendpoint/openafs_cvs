4d01daf562efdc6b4c4644d21e396f0f5da659f1
shadow <shadow>
2005-04-14 04:29:34 +0000

    DELTA renamefile-for-mrafslogs-20050414 AUTHOR shadow@dementia.org
    call the renamefile wrapper so mrafs style logs on windows work like other logs

--- src/util/serverLog.c
+++ src/util/serverLog.c
@@ -20,7 +20,7 @@
 #include <afs/param.h>
 
 RCSID
-    ("$Header: /cvs/openafs/src/util/serverLog.c,v 1.23 2004/09/08 21:35:53 jaltman Exp $");
+    ("$Header: /cvs/openafs/src/util/serverLog.c,v 1.24 2005/04/14 04:29:34 shadow Exp $");
 
 #include <stdio.h>
 #ifdef AFS_NT40_ENV
@@ -247,7 +247,7 @@ OpenLog(const char *fileName)
 		     TimeFields->tm_mon + 1, TimeFields->tm_mday,
 		     TimeFields->tm_hour, TimeFields->tm_min,
 		     TimeFields->tm_sec);
-	rename(fileName, FileName);	/* don't check error code */
+	renamefile(fileName, FileName);	/* don't check error code */
 	tempfd = open(fileName, O_WRONLY | O_TRUNC | O_CREAT, 0666);
     } else {
 	strcpy(oldName, fileName);
