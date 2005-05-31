810111c88a8362c5eda9e24321735519deb20896
shadow <shadow>
2005-05-31 19:55:35 +0000

    DELTA rxkad-stats-cleanup-20050531 AUTHOR shadow@dementia.org
    define this in kernel

--- src/rxkad/rxkad_common.c
+++ src/rxkad/rxkad_common.c
@@ -23,7 +23,7 @@
 #define INCLUDE_RXKAD_PRIVATE_DECLS
 
 RCSID
-    ("$Header: /cvs/openafs/src/rxkad/rxkad_common.c,v 1.26 2005/05/30 17:34:30 shadow Exp $");
+    ("$Header: /cvs/openafs/src/rxkad/rxkad_common.c,v 1.27 2005/05/31 19:55:35 shadow Exp $");
 
 #ifdef KERNEL
 #ifndef UKERNEL
@@ -89,8 +89,11 @@ struct rxkad_global_stats rxkad_global_stats = { 0 };
 pthread_mutex_t rxkad_global_stats_lock;
 pthread_key_t rxkad_stats_key;
 #else /* AFS_PTHREAD_ENV */
+#if defined(KERNEL) && !defined(UKERNEL)
+struct rxkad_stats rxkad_stats = { { 0 } }; 
+#else
 /* Move delaration of this to des/key_sched.c */
-/* struct rxkad_stats rxkad_stats = { { 0 } }; */
+#endif
 #endif /* AFS_PTHREAD_ENV */
 
 #ifdef AFS_PTHREAD_ENV
