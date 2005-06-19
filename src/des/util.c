e59b6e55b5e86f8f0247fd8bb5814ceefcaacfc0
shadow <shadow>
2005-06-19 00:14:14 +0000

    DELTA rxkad-stats-do-it-right-20050618 AUTHOR shadow@dementia.org
    fine, let's just do this right

--- src/des/util.c
+++ src/des/util.c
@@ -11,7 +11,7 @@
 #include <afs/param.h>
 
 RCSID
-    ("$Header: /cvs/openafs/src/des/util.c,v 1.8 2005/06/18 23:52:48 shadow Exp $");
+    ("$Header: /cvs/openafs/src/des/util.c,v 1.9 2005/06/19 00:14:17 shadow Exp $");
 
 #include "mit-cpyright.h"
 #ifndef KERNEL
@@ -20,11 +20,6 @@ RCSID
 #include <sys/types.h>
 #include <des.h>
 #include "des_prototypes.h"
-#include "stats.h"
-
-#ifndef AFS_PTHREAD_ENV 
-struct rxkad_stats rxkad_stats = { { 0 } }; 
-#endif
 
 int
 des_cblock_print_file(des_cblock * x, FILE * fp)
