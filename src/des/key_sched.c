90e84a93be7b7a3b06f4afadf777295cc3cb7b8f
shadow <shadow>
2005-06-21 20:19:48 +0000

    DELTA STABLE14-move-rxkad-stats-20050618 AUTHOR shadow@dementia.org
    fine, let's try this again, so aklog doesn't lose, but this sucks

--- src/des/key_sched.c
+++ src/des/key_sched.c
@@ -31,7 +31,7 @@
 #include <afs/param.h>
 
 RCSID
-    ("$Header: /cvs/openafs/src/des/key_sched.c,v 1.6.2.3 2005/05/30 18:30:36 shadow Exp $");
+    ("$Header: /cvs/openafs/src/des/key_sched.c,v 1.6.2.4 2005/06/21 20:19:48 shadow Exp $");
 
 #include <mit-cpyright.h>
 #include "des_internal.h"
@@ -43,10 +43,6 @@ RCSID
 #include "des_prototypes.h"
 
 typedef char key[64];
-
-#ifndef AFS_PTHREAD_ENV 
-struct rxkad_stats rxkad_stats = { { 0 } }; 
-#endif
 
 /* the following are really void but cc86 doesnt allow it */
 static int make_key_sched();
