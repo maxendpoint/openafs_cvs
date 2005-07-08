7af5049cc0aec5e1bd9e7e2a69a85740d8dd0d6c
rees <rees>
2005-07-08 16:53:43 +0000

    DELTA freebsd60-20050708 AUTHOR rees@umich.edu
    Fixes for FreeBSD 6.0 snap5.
    Builds and runs but has locking issues.

--- src/afs/FBSD/osi_vfsops.c
+++ src/afs/FBSD/osi_vfsops.c
@@ -2,7 +2,7 @@
 #include <afs/param.h>
 
 RCSID
-    ("$Header: /cvs/openafs/src/afs/FBSD/osi_vfsops.c,v 1.20 2005/04/29 21:55:58 rees Exp $");
+    ("$Header: /cvs/openafs/src/afs/FBSD/osi_vfsops.c,v 1.21 2005/07/08 16:53:43 rees Exp $");
 
 #include <afs/sysincludes.h>	/* Standard vendor system headers */
 #include <afsincludes.h>	/* Afs-based standard headers */
@@ -155,7 +155,9 @@ afs_unmount(struct mount *mp, int flags, THREAD_OR_PROC)
 }
 
 int
-#ifdef AFS_FBSD53_ENV
+#if defined(AFS_FBSD60_ENV)
+afs_root(struct mount *mp, int flags, struct vnode **vpp, struct thread *td)
+#elif defined(AFS_FBSD53_ENV)
 afs_root(struct mount *mp, struct vnode **vpp, struct thread *td)
 #else
 afs_root(struct mount *mp, struct vnode **vpp)
@@ -224,7 +226,7 @@ tryagain:
 	*vpp = vp;
     }
 
-    afs_Trace2(afs_iclSetp, CM_TRACE_VFSROOT, ICL_TYPE_POINTER, *vpp,
+    afs_Trace2(afs_iclSetp, CM_TRACE_VFSROOT, ICL_TYPE_POINTER, tvp ? AFSTOV(tvp) : NULL,
 	       ICL_TYPE_INT32, error);
     AFS_GUNLOCK();
     crfree(cr);
