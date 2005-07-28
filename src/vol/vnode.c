fdcf67f99de7a38a3f70540cfae0cd5868250e1d
shadow <shadow>
2005-07-28 21:11:19 +0000

    DELTA STABLE14-vnrehash-avoid-cachecheck-change-20050618 AUTHOR shadow@dementia.org
    unlocking and locking lets us race and makes cacheCheck potentially go bad

--- src/vol/vnode.c
+++ src/vol/vnode.c
@@ -17,7 +17,7 @@
 #include <afs/param.h>
 
 RCSID
-    ("$Header: /cvs/openafs/src/vol/vnode.c,v 1.19.2.3 2005/06/21 20:19:41 shadow Exp $");
+    ("$Header: /cvs/openafs/src/vol/vnode.c,v 1.19.2.4 2005/07/28 21:11:19 shadow Exp $");
 
 #include <errno.h>
 #include <stdio.h>
@@ -350,8 +350,10 @@ VAllocVnode_r(Error * ec, Volume * vp, VnodeType type)
 	    VOL_UNLOCK;
 	    ObtainWriteLock(&vnp->lock);
 	    VOL_LOCK;
-	    if (vnp->volumePtr->cacheCheck != vnp->cacheCheck)
+	    if (vnp->volumePtr->cacheCheck != vnp->cacheCheck) {
+		ReleaseWriteLock(&vnp->lock);
 		goto vnrehash;
+	    }
 	}
 #ifdef AFS_PTHREAD_ENV
 	vnp->writer = pthread_self();
