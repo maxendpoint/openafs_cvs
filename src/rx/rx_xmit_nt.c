c7bcf53dcc2bbda3b39e8d5b2a3660639b790e53
jaltman <jaltman>
2005-09-16 02:27:15 +0000

    DELTA rx-send-error-code-propagation-20050915 AUTHOR jaltman@secure-endpoints.com
    * add a version of rxi_DebugPrint for Windows that uses OutputDebugString
    
    * migrate all printf statements to the dpf macro
    
    * stop masking the errors from rx_sendmsg() so that higher level functions
      can make decisions based upon the failure.
    
    * Windows reports EHOSTUNREACHABLE.  Similar to Linux, if it is reported
      reset the send packet start time to 0 in order to immediately cause the
      server to be marked down.

--- src/rx/rx_xmit_nt.c
+++ src/rx/rx_xmit_nt.c
@@ -18,7 +18,7 @@
 #include <afs/param.h>
 
 RCSID
-    ("$Header: /cvs/openafs/src/rx/rx_xmit_nt.c,v 1.8 2004/08/19 00:41:54 kolya Exp $");
+    ("$Header: /cvs/openafs/src/rx/rx_xmit_nt.c,v 1.9 2005/09/16 02:27:16 jaltman Exp $");
 
 #if defined(AFS_NT40_ENV) || defined(AFS_DJGPP_ENV)
 
@@ -139,12 +139,15 @@ sendmsg(int socket, struct msghdr *msgP, int flags)
 	case WSAEWOULDBLOCK:
 	    errno = WSAEWOULDBLOCK;
 	    break;
+	case WSAEHOSTUNREACH:
+		errno = WSAEHOSTUNREACH;
+		break;
 	default:
 	    errno = EIO;
 	    break;
 	}
 	code = -1;
-    }
+    } else
 #endif /* AFS_NT40_ENV */
 
     if (code < size) {
