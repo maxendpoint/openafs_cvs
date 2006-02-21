f17c14e6b4feb459fdf7992b76e5155a0079bfcf
shadow <shadow>
2006-02-21 04:47:07 +0000

    DELTA STABLE14-tiger-updates-20060220 AUTHOR cg2v@andrew.cmu.edu
    update lpioctl, setattr and free tuios

--- src/afs/DARWIN/osi_file.c
+++ src/afs/DARWIN/osi_file.c
@@ -11,7 +11,7 @@
 #include "afs/param.h"
 
 RCSID
-    ("$Header: /cvs/openafs/src/afs/DARWIN/osi_file.c,v 1.8.2.3 2005/12/01 07:37:21 shadow Exp $");
+    ("$Header: /cvs/openafs/src/afs/DARWIN/osi_file.c,v 1.8.2.4 2006/02/21 04:47:07 shadow Exp $");
 
 #include "afs/sysincludes.h"	/* Standard vendor system headers */
 #include "afsincludes.h"	/* Afs-based standard headers */
@@ -247,7 +247,7 @@ osi_UFSTruncate(register struct osi_file *afile, afs_int32 asize)
 #ifdef AFS_DARWIN80_ENV
     VATTR_INIT(&tvattr);
     VATTR_SET(&tvattr, va_size, asize);
-    code = vnode_getattr(afile->vnode, &tvattr, afs_osi_ctxtp);
+    code = vnode_setattr(afile->vnode, &tvattr, afs_osi_ctxtp);
 #else
     VATTR_NULL(&tvattr);
     tvattr.va_size = asize;
