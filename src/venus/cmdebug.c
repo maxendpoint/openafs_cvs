9358e175414bb100afa96c295591c71ffe7ff9b3
shadow <shadow>
2006-03-23 16:43:28 +0000

    DELTA STABLE14-print-64-20051117 AUTHOR rees@umich.edu
    On non-Windows use %llu for 64 bit printf.
    Thanks to Brent Graveland for pointing this out.

--- src/venus/cmdebug.c
+++ src/venus/cmdebug.c
@@ -11,7 +11,7 @@
 #include <afs/param.h>
 
 RCSID
-    ("$Header: /cvs/openafs/src/venus/cmdebug.c,v 1.15.2.2 2006/01/20 00:03:10 shadow Exp $");
+    ("$Header: /cvs/openafs/src/venus/cmdebug.c,v 1.15.2.3 2006/03/23 16:43:28 shadow Exp $");
 
 
 #include <sys/types.h>
@@ -399,8 +399,13 @@ PrintCacheEntries64(struct rx_connection *aconn, int aint32)
 	    printf("\n");
 	}
 #ifdef AFS_64BIT_ENV
+#ifdef AFS_NT40_ENV
 	printf("    %012I64d bytes  DV %012d  refcnt %05d\n", centry.Length,
 	       centry.DataVersion, centry.refCount);
+#else
+	printf("    %012llu bytes  DV %012d  refcnt %05d\n", centry.Length,
+	       centry.DataVersion, centry.refCount);
+#endif
 #else
 	printf("    %012d bytes  DV %012d  refcnt %05d\n", centry.Length,
 	       centry.DataVersion, centry.refCount);
