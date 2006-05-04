d3483d301ce24cc184422562436fbc99c15ffe3d
kenh <kenh>
2006-05-04 22:17:43 +0000

    DELTA more-rx-updates-20060504 AUTHOR kenh@cmf.nrl.navy.mil
    Suppress warnings from prototype changes.
    Trial code for OpenBSD support.
    Fix missing piece from UKERNEL.

--- src/afs/afs_mariner.c
+++ src/afs/afs_mariner.c
@@ -17,7 +17,7 @@
 #include "afs/param.h"
 
 RCSID
-    ("$Header: /cvs/openafs/src/afs/afs_mariner.c,v 1.9 2006/05/04 21:23:17 kenh Exp $");
+    ("$Header: /cvs/openafs/src/afs/afs_mariner.c,v 1.10 2006/05/04 22:17:43 kenh Exp $");
 
 #include "afs/sysincludes.h"	/*Standard vendor system headers */
 #include "afsincludes.h"	/*AFS-based standard headers */
@@ -99,8 +99,8 @@ afs_MarinerLogFetch(register struct vcache *avc, register afs_int32 off,
     dvec.iov_base = tp1;
     dvec.iov_len = len;
     AFS_GUNLOCK();
-    (void)osi_NetSend(afs_server->socket, &taddr, sizeof(taddr), &dvec, 1,
-		      len, 0);
+    (void)osi_NetSend(afs_server->socket, (struct sockaddr_storage *) &taddr,
+		      sizeof(taddr), &dvec, 1, len, 0);
     AFS_GLOCK();
     osi_FreeSmallSpace(tp1);
 }				/*afs_MarinerLogFetch */
@@ -133,8 +133,8 @@ afs_MarinerLog(register char *astring, register struct vcache *avc)
     dvec.iov_base = buf;
     dvec.iov_len = tp - buf;
     AFS_GUNLOCK();
-    (void)osi_NetSend(afs_server->socket, &taddr, sizeof(taddr), &dvec, 1,
-		      tp - buf, 0);
+    (void)osi_NetSend(afs_server->socket, (struct sockaddr_storage *) &taddr,
+		      sizeof(taddr), &dvec, 1, tp - buf, 0);
     AFS_GLOCK();
     osi_FreeSmallSpace(buf);
 }				/*afs_MarinerLog */
