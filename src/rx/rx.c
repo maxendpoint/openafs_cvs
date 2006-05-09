f1f4ff51147c6a1adec2cb0c0380c2283325ec43
jaltman <jaltman>
2006-05-09 04:15:47 +0000

    DELTA bringing-rx-into-21st-century-20060504 AUTHOR jaltman@secure-endpoints.com
    rx_InitHost() port is in network byte order

--- src/rx/rx.c
+++ src/rx/rx.c
@@ -17,7 +17,7 @@
 #endif
 
 RCSID
-    ("$Header: /cvs/openafs/src/rx/rx.c,v 1.98 2006/05/04 21:23:18 kenh Exp $");
+    ("$Header: /cvs/openafs/src/rx/rx.c,v 1.99 2006/05/09 04:15:47 jaltman Exp $");
 
 #ifdef KERNEL
 #include "afs/sysincludes.h"
@@ -375,6 +375,7 @@ static int rxinit_status = 1;
 
 /*
  * Now, rx_InitHost is just a stub for rx_InitAddrs
+ * Parameters are in network byte order.
  */
 
 int
@@ -386,7 +387,7 @@ rx_InitHost(u_int host, u_int port)
     memset((void *) &saddr, 0, sizeof(saddr));
     rx_ssfamily(&saddr) = AF_INET;
     ((struct sockaddr_in *) &saddr)->sin_addr.s_addr = host;
-    ((struct sockaddr_in *) &saddr)->sin_port = htons(port);
+    ((struct sockaddr_in *) &saddr)->sin_port = (u_short)port;
 #ifdef STRUCT_SOCKADDR_HAS_SA_LEN
     ((struct sockaddr_in *) &saddr)->sin_len = sizeof(struct sockaddr_in);
 #endif
