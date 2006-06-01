c63ef5496d167e6e9884d275a18719f159df8a12
shadow <shadow>
2006-06-01 15:36:58 +0000

    DELTA DEVEL15-viced-writevalloc-dont-vtakeoffline-20060510 AUTHOR shadow@dementia.org
    random errors shouldn't take the volume offline

--- src/viced/afsfileprocs.c
+++ src/viced/afsfileprocs.c
@@ -29,7 +29,7 @@
 #include <afs/param.h>
 
 RCSID
-    ("$Header: /cvs/openafs/src/viced/afsfileprocs.c,v 1.113.2.1 2006/06/01 15:31:52 shadow Exp $");
+    ("$Header: /cvs/openafs/src/viced/afsfileprocs.c,v 1.113.2.2 2006/06/01 15:36:58 shadow Exp $");
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6837,7 +6837,6 @@ FetchData_RXStyle(Volume * volptr, Vnode * targetptr,
 	errorCode = rx_WritevAlloc(Call, tiov, &tnio, RX_MAXIOVECS, wlen);
 	if (errorCode <= 0) {
 	    FDH_CLOSE(fdP);
-	    VTakeOffline(volptr);
 	    return EIO;
 	}
 	wlen = errorCode;
