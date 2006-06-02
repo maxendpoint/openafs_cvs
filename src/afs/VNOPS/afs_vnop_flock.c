2c81188144fb17964acce1b3c1e715a7eb48f80e
shadow <shadow>
2006-06-02 21:23:52 +0000

    DELTA STABLE14-flock-handle-uae-errors-20060601 AUTHOR cg2v@andrew.cmu.edu
    handle uae versions of errors for flock locks

--- src/afs/VNOPS/afs_vnop_flock.c
+++ src/afs/VNOPS/afs_vnop_flock.c
@@ -16,7 +16,7 @@
 #include "afs/param.h"
 
 RCSID
-    ("$Header: /cvs/openafs/src/afs/VNOPS/afs_vnop_flock.c,v 1.24.2.5 2006/06/01 16:02:31 shadow Exp $");
+    ("$Header: /cvs/openafs/src/afs/VNOPS/afs_vnop_flock.c,v 1.24.2.6 2006/06/02 21:23:52 shadow Exp $");
 
 #include "afs/sysincludes.h"	/* Standard vendor system headers */
 #include "afsincludes.h"	/* Afs-based standard headers */
@@ -24,6 +24,7 @@ RCSID
 #include "afs/afs_cbqueue.h"
 #include "afs/nfsclient.h"
 #include "afs/afs_osidnlc.h"
+#include "afs/unified_afs.h"
 
 /* Static prototypes */
 static int HandleGetLock(register struct vcache *avc,
