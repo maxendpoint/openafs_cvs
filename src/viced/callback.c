74c7826b4a51299e0cf9f9ff564c83bfc497657a
jaltman <jaltman>
2006-06-24 14:10:46 +0000

    DELTA DEVEL15-windows-viced-cbd-20060624 AUTHOR jaltman@secure-endpoints.com
    don't break platforms other than Windows

--- src/viced/callback.c
+++ src/viced/callback.c
@@ -85,7 +85,7 @@
 #include <afs/param.h>
 
 RCSID
-    ("$Header: /cvs/openafs/src/viced/callback.c,v 1.77.2.1 2006/06/20 22:10:53 jaltman Exp $");
+    ("$Header: /cvs/openafs/src/viced/callback.c,v 1.77.2.2 2006/06/24 14:10:46 jaltman Exp $");
 
 #include <stdio.h>
 #include <stdlib.h>		/* for malloc() */
@@ -2723,7 +2723,11 @@ ReadDump(char *file)
     return now;
 }
 
+#ifdef AFS_NT40_ENV
 #include "AFS_component_version_number.h"
+#else
+#include "AFS_component_version_number.c"
+#endif
 
 int
 main(int argc, char **argv)
