5d8353f5c656b04790d5019184b0109e508c1c1c
shadow <shadow>
2006-08-13 16:41:54 +0000

    DELTA STABLE14-fix-congestion-tracking-20060813 AUTHOR chas@cmf.nrl.navy.mil FIXES 36951
    fix congestion tracking

--- src/rx/rx.c
+++ src/rx/rx.c
@@ -17,7 +17,7 @@
 #endif
 
 RCSID
-    ("$Header: /cvs/openafs/src/rx/rx.c,v 1.58.2.34 2006/07/03 01:16:36 jaltman Exp $");
+    ("$Header: /cvs/openafs/src/rx/rx.c,v 1.58.2.35 2006/08/13 16:41:54 shadow Exp $");
 
 #ifdef KERNEL
 #include "afs/sysincludes.h"
@@ -3710,10 +3710,11 @@ rxi_ReceiveAckPacket(register struct rx_call *call, struct rx_packet *np,
 	 * be unable to accept packets of the size that prior AFS versions would
 	 * send without asking.  */
 	if (peer->maxMTU != tSize) {
+	    if (peer->maxMTU > tSize) /* possible cong., maxMTU decreased */
+		peer->congestSeq++;
 	    peer->maxMTU = tSize;
 	    peer->MTU = MIN(tSize, peer->MTU);
 	    call->MTU = MIN(call->MTU, tSize);
-	    peer->congestSeq++;
 	}
 
 	if (np->length == rx_AckDataSize(ap->nAcks) + 3 * sizeof(afs_int32)) {
