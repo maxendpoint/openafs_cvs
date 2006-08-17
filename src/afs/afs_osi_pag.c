964faa54d4142b96d424057df879dc381938ea22
shadow <shadow>
2006-08-17 13:55:43 +0000

    DELTA linux-keyring-pag-update-20060817 AUTHOR chas@cmf.nrl.navy.mil FIXES 37887
    if you manage to patch the syscall table, then the setpag() in the
    patched setgroups syscall and creates a new session keyring. it does
    contain the right pag id but this isnt the intended behavior.
    
    so setpag() becomes __setpag() which just does the group insertion.
    (and perhaps it should be called something else?). the stat count for
    the setpag operation might be more correct now (instead of also counting
    the pag restores).

--- src/afs/afs_osi_pag.c
+++ src/afs/afs_osi_pag.c
@@ -23,7 +23,7 @@
 #include "afs/param.h"
 
 RCSID
-    ("$Header: /cvs/openafs/src/afs/afs_osi_pag.c,v 1.31 2006/08/04 15:55:23 shadow Exp $");
+    ("$Header: /cvs/openafs/src/afs/afs_osi_pag.c,v 1.32 2006/08/17 13:55:43 shadow Exp $");
 
 #include "afs/sysincludes.h"	/* Standard vendor system headers */
 #include "afsincludes.h"	/* Afs-based standard headers */
@@ -539,7 +539,7 @@ PagInCred(const struct AFS_UCRED *cred)
 #endif
     pag = (afs_int32) afs_get_pag_from_groups(g0, g1);
 out:
-#ifdef LINUX_KEYRING_SUPPORT
+#if defined(AFS_LINUX26_ENV) && defined(LINUX_KEYRING_SUPPORT)
     if (pag == NOPAG) {
 	struct key *key;
 	afs_uint32 pag, newpag;
