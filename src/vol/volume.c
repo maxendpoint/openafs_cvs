6184e02c9cf5d39595667f40c996c000743d6331
shadow <shadow>
2006-08-24 16:40:52 +0000

    DELTA vol-init-locallock-20060824 AUTHOR jhutz@cmu.edu
    we never init this. oops.

--- src/vol/volume.c
+++ src/vol/volume.c
@@ -22,7 +22,7 @@
 #include <afs/param.h>
 
 RCSID
-    ("$Header: /cvs/openafs/src/vol/volume.c,v 1.45 2006/08/03 02:57:07 rra Exp $");
+    ("$Header: /cvs/openafs/src/vol/volume.c,v 1.46 2006/08/24 16:40:52 shadow Exp $");
 
 #include <rx/xdr.h>
 #include <afs/afsint.h>
@@ -201,6 +201,7 @@ int LogLevel;			/* Vice loglevel--not defined as extern so that it will be
 				 * defined when not linked with vice, XXXX */
 ProgramType programType;	/* The type of program using the package */
 
+extern struct Lock localLock;
 
 /* extended volume package statistics */
 VolPkgStats VStats;
@@ -460,6 +461,7 @@ VInitVolumePackage(ProgramType pt, afs_uint32 nLargeVnodes, afs_uint32 nSmallVno
     IOMGR_Initialize();
 #endif /* AFS_PTHREAD_ENV */
     Lock_Init(&vol_listLock);
+    Lock_Init(&localLock);
 
     srandom(time(0));		/* For VGetVolumeInfo */
     gettimeofday(&tv, &tz);
