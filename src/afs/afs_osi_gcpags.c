d2b2a8a41960def94b806eebaf4dc323c7ed126a
shadow <shadow>
2006-09-22 11:18:37 +0000

    DELTA DEVEL15-tasklist-lock-redux-20060922 AUTHOR jhutz@cmu.edu
    try tasklist_lock weak binding in osi_probe
    
    if no tasklist_lock, fall back to rcu locking

--- src/afs/afs_osi_gcpags.c
+++ src/afs/afs_osi_gcpags.c
@@ -11,7 +11,7 @@
 #include "afs/param.h"
 
 RCSID
-    ("$Header: /cvs/openafs/src/afs/afs_osi_gcpags.c,v 1.1.2.2 2006/07/31 21:27:38 shadow Exp $");
+    ("$Header: /cvs/openafs/src/afs/afs_osi_gcpags.c,v 1.1.2.3 2006/09/22 11:18:37 shadow Exp $");
 
 #include "afs/sysincludes.h"	/* Standard vendor system headers */
 #include "afsincludes.h"	/* Afs-based standard headers */
@@ -233,6 +233,11 @@ afs_osi_TraverseProcTable()
     struct task_struct *p;
     if (&tasklist_lock)
        read_lock(&tasklist_lock);
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,16)
+    else
+	rcu_read_lock();
+#endif
+
 #ifdef DEFINED_FOR_EACH_PROCESS
     for_each_process(p) if (p->pid) {
 #ifdef STRUCT_TASK_STRUCT_HAS_EXIT_STATE
@@ -258,6 +263,10 @@ afs_osi_TraverseProcTable()
 #endif
     if (&tasklist_lock)
        read_unlock(&tasklist_lock);
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,16)
+    else
+	rcu_read_unlock();
+#endif
 }
 #endif
 
