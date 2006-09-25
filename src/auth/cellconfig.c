e7e7e6bb0b607a631c5cdf8e8ac3d661e0a4ef55
shadow <shadow>
2006-09-25 15:04:12 +0000

    DELTA getcell-still-stupid-20060925 AUTHOR kenaaker@myclearwave.net FIXES 40829
    should be fixed a third time too

--- src/auth/cellconfig.c
+++ src/auth/cellconfig.c
@@ -11,7 +11,7 @@
 #include <afs/param.h>
 
 RCSID
-    ("$Header: /cvs/openafs/src/auth/cellconfig.c,v 1.49 2006/09/17 05:01:43 shadow Exp $");
+    ("$Header: /cvs/openafs/src/auth/cellconfig.c,v 1.50 2006/09/25 15:04:12 shadow Exp $");
 
 #include <afs/stds.h>
 #include <afs/pthread_glock.h>
@@ -496,7 +496,8 @@ GetCellUnix(struct afsconf_dir *adir)
 		*p = '\0';
 
             adir->cellName = (char *)malloc(sz + 1);
-            strncpy(adir->cellName, tbuffer, sz);
+            memset(adir->cellName, 0, sz+1);
+	    strncpy(adir->cellName, tbuffer, sz);
         }
     }
     return 0;
