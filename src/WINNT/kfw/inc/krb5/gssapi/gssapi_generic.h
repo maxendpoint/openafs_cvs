ad5dce66e6462d8637b014fa9444bc3ac79f4d40
jaltman <jaltman>
2006-10-12 21:42:20 +0000

    DELTA DEVEL15-windows-kfw-sdk-20060921 AUTHOR jaltman@secure-endpoints.com
    kfw 3.1

--- src/WINNT/kfw/inc/krb5/gssapi/gssapi_generic.h
+++ src/WINNT/kfw/inc/krb5/gssapi/gssapi_generic.h
@@ -24,7 +24,7 @@
 #define _GSSAPI_GENERIC_H_
 
 /*
- * $Id: gssapi_generic.h,v 1.2 2005/03/11 05:37:54 jaltman Exp $
+ * $Id: gssapi_generic.h,v 1.2.6.1 2006/10/12 21:42:39 jaltman Exp $
  */
 
 #include <gssapi/gssapi.h>
