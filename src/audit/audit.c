43a1a762003db176eee5c7d20a3ad04d204cbd41
shadow <shadow>
2006-10-13 19:43:45 +0000

    DELTA DEVEL15-audit-one-more-thing-20061013 AUTHOR shadow@dementia.org
    it never ends

--- src/audit/audit.c
+++ src/audit/audit.c
@@ -11,7 +11,7 @@
 #include <afs/param.h>
 
 RCSID
-    ("$Header: /cvs/openafs/src/audit/audit.c,v 1.20.2.3 2006/10/13 17:39:06 shadow Exp $");
+    ("$Header: /cvs/openafs/src/audit/audit.c,v 1.20.2.4 2006/10/13 19:43:45 shadow Exp $");
 
 #include <fcntl.h>
 #include <stdarg.h>
@@ -36,6 +36,16 @@ RCSID
 #include <sys/audit.h>
 #endif
 #include <afs/afsutil.h>
+
+/* C99 requires va_copy.  Older versions of GCC provide __va_copy.  Per t
+   Autoconf manual, memcpy is a generally portable fallback. */          
+#ifndef va_copy              
+# ifdef __va_copy
+#  define va_copy(d, s)         __va_copy((d), (s))             
+# else
+#  define va_copy(d, s)         memcpy(&(d), &(s), sizeof(va_list)) 
+# endif
+#endif      
 
 char *bufferPtr;
 int bufferLen;
