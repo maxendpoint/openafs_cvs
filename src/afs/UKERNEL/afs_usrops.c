0b4b53019cf6f5b73c4449ebf301ae4fe8dce765
shadow <shadow>
2006-11-10 04:02:01 +0000

    DELTA kill-stat-blksize-20061109 AUTHOR chas@cmf.nrl.navy.mil FIXES 42663
    don't provide a bogus hint

--- src/afs/UKERNEL/afs_usrops.c
+++ src/afs/UKERNEL/afs_usrops.c
@@ -15,7 +15,7 @@
 #include "afs/param.h"
 
 RCSID
-    ("$Header: /cvs/openafs/src/afs/UKERNEL/afs_usrops.c,v 1.31 2006/07/31 21:17:42 shadow Exp $");
+    ("$Header: /cvs/openafs/src/afs/UKERNEL/afs_usrops.c,v 1.32 2006/11/10 04:02:01 shadow Exp $");
 
 
 #ifdef	UKERNEL
@@ -854,7 +854,6 @@ afs_osi_Stat(struct osi_file *fp, struct osi_stat *stp)
 	return -1;
     }
     stp->size = st.st_size;
-    stp->blksize = st.st_blksize;
     stp->mtime = st.st_mtime;
     stp->atime = st.st_atime;
     AFS_GLOCK();
