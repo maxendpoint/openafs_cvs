a9a3478be63916278f2d450e3ccb542617cf7a23
jaltman <jaltman>
2006-11-28 09:29:28 +0000

    DELTA volser-cleanup-20061128 AUTHOR jaltman@secure-endpoints.com
    cleanup the return code handling from
    
      DELTA volser-dump-validate-input-20060417

--- src/volser/volprocs.c
+++ src/volser/volprocs.c
@@ -11,7 +11,7 @@
 #include <afs/param.h>
 
 RCSID
-    ("$Header: /cvs/openafs/src/volser/volprocs.c,v 1.42 2006/03/17 19:54:56 shadow Exp $");
+    ("$Header: /cvs/openafs/src/volser/volprocs.c,v 1.42.2.1 2006/11/28 09:29:28 jaltman Exp $");
 
 #include <stdio.h>
 #include <sys/types.h>
@@ -1197,8 +1197,8 @@ SAFSVolForwardMultiple(struct rx_call *acid, afs_int32 fromTrans, afs_int32
     vp = tt->volume;
     strcpy(tt->lastProcName, "ForwardMulti");
 
-    /* (fromDate == 0) ==> incremental dump */
-    is_incremental = (fromDate ? 1 : 0);
+    /* (fromDate == 0) ==> full dump */
+    is_incremental = ((V_parentId(vp) == V_id(vp) && fromDate) ? 1 : 0);
 
     i = results->manyResults_len = destinations->manyDests_len;
     results->manyResults_val = codes =
