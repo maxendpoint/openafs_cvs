8f4f7ebab7642521decebf4e8a7f83c47e422545
shadow <shadow>
2006-12-19 04:50:46 +0000

    DELTA DEVEL15-volser-dumpstuff-20061218 AUTHOR reuter@rzg.mpg.de FIXES 46937
    "vos release" per default does an incremental dump.
    volser-dump-validate-input-20060417 ends up with keeping dead vnodes and
    data in the remote RO-volume.  This patch corrects the behavior for
    RO-volumes.
    
    See ticket for extensive discussion.

--- src/volser/dumpstuff.c
+++ src/volser/dumpstuff.c
@@ -11,7 +11,7 @@
 #include <afs/param.h>
 
 RCSID
-    ("$Header: /cvs/openafs/src/volser/dumpstuff.c,v 1.29.2.5 2006/12/19 03:54:27 jaltman Exp $");
+    ("$Header: /cvs/openafs/src/volser/dumpstuff.c,v 1.29.2.6 2006/12/19 04:50:46 shadow Exp $");
 
 #include <sys/types.h>
 #include <ctype.h>
@@ -1041,7 +1041,7 @@ RestoreVolume(register struct rx_call *call, Volume * avp, int incremental,
     iod_Init(iodp, call);
 
     vp = avp;
-    if (V_parentId(vp) == V_id(p))
+    if (V_parentId(vp) == V_id(vp))
       delo = incremental;
 
     if (!ReadDumpHeader(iodp, &header)) {
