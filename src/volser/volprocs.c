5526b6dcb74a68b445d5fe097ce41fa8a61f1eb4
jaltman <jaltman>
2006-12-19 03:37:34 +0000

    DELTA volser-undo-cleanup-20061218 AUTHOR jaltman@secure-endpoints.com
    uncommit an inappropriate commit
    
      DELTA volser-cleanup-xx-20061128

--- src/volser/volprocs.c
+++ src/volser/volprocs.c
@@ -11,7 +11,7 @@
 #include <afs/param.h>
 
 RCSID
-    ("$Header: /cvs/openafs/src/volser/volprocs.c,v 1.43 2006/11/28 09:30:58 jaltman Exp $");
+    ("$Header: /cvs/openafs/src/volser/volprocs.c,v 1.44 2006/12/19 03:37:34 jaltman Exp $");
 
 #include <stdio.h>
 #include <sys/types.h>
@@ -1198,7 +1198,7 @@ SAFSVolForwardMultiple(struct rx_call *acid, afs_int32 fromTrans, afs_int32
     strcpy(tt->lastProcName, "ForwardMulti");
 
     /* (fromDate == 0) ==> full dump */
-    is_incremental = ((V_parentId(vp) == V_id(vp) && fromDate) ? 1 : 0);
+    is_incremental = (fromDate ? 1 : 0);
 
     i = results->manyResults_len = destinations->manyDests_len;
     results->manyResults_val = codes =
