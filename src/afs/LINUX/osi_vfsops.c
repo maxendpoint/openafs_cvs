6ec9bb8dde4a4698f30c3525a44f146b5fe5792e
shadow <shadow>
2006-12-28 21:28:31 +0000

    DELTA linux-2620-rc1-update-20061228 AUTHOR marc.dionne@technoconseil.com FIXES 49890
    updates for linux 2.6.20-rc1

--- src/afs/LINUX/osi_vfsops.c
+++ src/afs/LINUX/osi_vfsops.c
@@ -16,7 +16,7 @@
 #include "afs/param.h"
 
 RCSID
-    ("$Header: /cvs/openafs/src/afs/LINUX/osi_vfsops.c,v 1.52 2006/11/10 00:06:50 shadow Exp $");
+    ("$Header: /cvs/openafs/src/afs/LINUX/osi_vfsops.c,v 1.53 2006/12/28 21:29:02 shadow Exp $");
 
 #define __NO_VERSION__		/* don't define kernel_version in module.h */
 #include <linux/module.h> /* early to avoid printf->printk mapping */
@@ -276,7 +276,11 @@ afs_alloc_inode(struct super_block *sb)
 {
     struct vcache *vcp;
 
+#if defined(SLAB_KERNEL)
     vcp = (struct vcache *) kmem_cache_alloc(afs_inode_cachep, SLAB_KERNEL);
+#else
+    vcp = (struct vcache *) kmem_cache_alloc(afs_inode_cachep, GFP_KERNEL);
+#endif
     if (!vcp)
 	return NULL;
 
