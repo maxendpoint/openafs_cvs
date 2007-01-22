43d8d4d48c5d0fe15b15834c5bc280a16326c411
rees <rees>
2007-01-22 22:30:15 +0000

    DELTA asetkey-includes-20060122 AUTHOR rees@umich.edu
    fix includes

--- src/aklog/asetkey.c
+++ src/aklog/asetkey.c
@@ -1,5 +1,5 @@
 /*
- * $Id: asetkey.c,v 1.6 2007/01/05 03:21:34 shadow Exp $
+ * $Id: asetkey.c,v 1.7 2007/01/22 22:30:15 rees Exp $
  *
  * asetkey - Manipulates an AFS KeyFile
  *
@@ -7,10 +7,10 @@
  */
 
 #include <afsconfig.h>
-#include <stdio.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <stdio.h>
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
@@ -31,7 +31,9 @@
 #include <afs/stds.h>
 #include <krb5.h>
 
+#ifndef HAVE_KERBEROSV_HEIM_ERR_H
 #include <afs/com_err.h>
+#endif
 #include <afs/cellconfig.h>
 #include <afs/keys.h>
 #include <afs/dirpath.h>
