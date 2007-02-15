5d2f79e19b215a88bd02df1e415bc45ecfeba9a6
shadow <shadow>
2007-02-15 00:33:39 +0000

    DELTA java-locking-redux-20070214 AUTHOR megacz@cs.berkeley.edu FIXES
    change the -1 len check for java locks slightly

--- src/afs/VNOPS/afs_vnop_flock.c
+++ src/afs/VNOPS/afs_vnop_flock.c
@@ -16,7 +16,7 @@
 #include "afs/param.h"
 
 RCSID
-    ("$Header: /cvs/openafs/src/afs/VNOPS/afs_vnop_flock.c,v 1.33 2007/02/13 01:33:20 shadow Exp $");
+    ("$Header: /cvs/openafs/src/afs/VNOPS/afs_vnop_flock.c,v 1.34 2007/02/15 00:33:39 shadow Exp $");
 
 #include "afs/sysincludes.h"	/* Standard vendor system headers */
 #include "afsincludes.h"	/* Afs-based standard headers */
@@ -25,6 +25,10 @@ RCSID
 #include "afs/nfsclient.h"
 #include "afs/afs_osidnlc.h"
 #include "afs/unified_afs.h"
+
+#ifndef OFFSET_MAX
+#define OFFSET_MAX 0x7fffffffffffffff
+#endif
 
 /* Static prototypes */
 static int HandleGetLock(register struct vcache *avc,
@@ -549,7 +553,7 @@ int afs_lockctl(struct vcache * avc, struct AFS_FLOCK * af, int acmd,
 #endif
     /* Java VMs ask for l_len=(long)-1 regardless of OS/CPU; bottom 32 bits
      * sometimes get masked off by OS */
-    if ((af->l_len >> 32) == 0x7fffffff)
+    if (af->l_len == OFFSET_MAX-1)
 	af->l_len = 0;
     /* next line makes byte range locks always succeed,
      * even when they should block */
