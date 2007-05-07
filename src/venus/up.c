e670c6feb36bb511f65bd0ee3b2c896ecb8f9653
rra <rra>
2007-05-07 17:17:42 +0000

    DELTA up-preserve-dir-mtime-20070507 AUTHOR rra@stanford.edu FIXES 34522
    Set the mtime on newly created directories to match the original directory.

--- src/venus/up.c
+++ src/venus/up.c
@@ -11,7 +11,7 @@
 #include <afs/param.h>
 
 RCSID
-    ("$Header: /cvs/openafs/src/venus/up.c,v 1.18 2006/12/20 17:03:59 rees Exp $");
+    ("$Header: /cvs/openafs/src/venus/up.c,v 1.19 2007/05/07 17:17:42 rra Exp $");
 
 /* missing type from C language */
 
@@ -511,6 +511,7 @@ Copy(char *file1, char *file2, short recursive, int level)
 	char f1[MAXPATHLEN], f2[MAXPATHLEN];
 	char *p1, *p2;
 	struct dirent *d;
+	struct timeval tv[2];
 
 	if (verbose) {
 	    printf("Level %d: Directory %s to %s\n", level, file1, file2);
@@ -698,6 +699,15 @@ Copy(char *file1, char *file2, short recursive, int level)
 		printf("Not setting acls\n");
 	    }
 	}
+
+        /* preserve access and modification times: ("-x" disables) */
+        if (preserveDate) {
+            tv[0].tv_sec = s1.st_atime;
+            tv[0].tv_usec = 0;
+            tv[1].tv_sec = s1.st_mtime;
+            tv[1].tv_usec = 0;
+            utimes(file2, tv);
+        }
     }
 
     return rcode;
