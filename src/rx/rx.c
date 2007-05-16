57885d331d44f2302ea7027eb2914346f84a9b71
shadow <shadow>
2007-05-16 20:39:09 +0000

    DELTA STABLE14-rx-send-req-ack-20070516 AUTHOR cg2v@andrew.cmu.edu FIXES 60046
    change reply code where appropriate

--- src/rx/rx.c
+++ src/rx/rx.c
@@ -17,7 +17,7 @@
 #endif
 
 RCSID
-    ("$Header: /cvs/openafs/src/rx/rx.c,v 1.58.2.37 2007/04/25 19:50:21 shadow Exp $");
+    ("$Header: /cvs/openafs/src/rx/rx.c,v 1.58.2.38 2007/05/16 20:39:09 shadow Exp $");
 
 #ifdef KERNEL
 #include "afs/sysincludes.h"
@@ -3346,9 +3346,11 @@ rxi_ReceiveDataPacket(register struct rx_call *call,
 
 	    /* We need to send an ack of the packet is out of sequence, 
 	     * or if an ack was requested by the peer. */
-	    if (seq != prev + 1 || missing || (flags & RX_REQUEST_ACK)) {
+	    if (seq != prev + 1 || missing) {
 		ackNeeded = RX_ACK_OUT_OF_SEQUENCE;
-	    }
+	    } else if (flags & RX_REQUEST_ACK) {
+		ackNeeded = RX_ACK_REQUESTED;
+            }
 
 	    /* Acknowledge the last packet for each call */
 	    if (flags & RX_LAST_PACKET) {
