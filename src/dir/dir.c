914b68f48c74771b8b693a6d5f2565481ac9781f
shadow <shadow>
2007-10-11 13:59:47 +0000

    DELTA DEVEL15-gcc42-integer-overflow-fix-20071007 AUTHOR marc.dionne@technoconseil.com
    with fix from Hans-Werner Pulsen

--- src/dir/dir.c
+++ src/dir/dir.c
@@ -11,7 +11,7 @@
 #include <afs/param.h>
 
 RCSID
-    ("$Header: /cvs/openafs/src/dir/dir.c,v 1.24.4.1 2007/10/07 20:01:45 shadow Exp $");
+    ("$Header: /cvs/openafs/src/dir/dir.c,v 1.24.4.2 2007/10/11 13:59:47 shadow Exp $");
 
 #ifdef KERNEL
 #if !defined(UKERNEL)
@@ -478,8 +478,8 @@ DirHash(register char *string)
 {
     /* Hash a string to a number between 0 and NHASHENT. */
     register unsigned char tc;
-    unsigned long hval;
-    register int tval;
+    unsigned int hval;
+    register int tval, hval;
     hval = 0;
     while ((tc = (*string++))) {
 	hval *= 173;
