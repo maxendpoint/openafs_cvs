7819e72f4b3b61bc10b158202242b77f33e2a18e
shadow <shadow>
2008-03-13 02:23:57 +0000

    DELTA linux-2625-20080312 AUTHOR marc.dionne@technoconseil.com LICENSE IPL10 FIXES
    add missing bits

--- src/afs/LINUX/osi_file.c
+++ src/afs/LINUX/osi_file.c
@@ -11,7 +11,7 @@
 #include "afs/param.h"
 
 RCSID
-    ("$Header: /cvs/openafs/src/afs/LINUX/osi_file.c,v 1.33 2008/03/10 18:50:51 shadow Exp $");
+    ("$Header: /cvs/openafs/src/afs/LINUX/osi_file.c,v 1.34 2008/03/13 02:23:57 shadow Exp $");
 
 #ifdef AFS_LINUX24_ENV
 #include "h/module.h" /* early to avoid printf->printk mapping */
@@ -41,6 +41,11 @@ osi_UFSOpen(afs_int32 ainode)
     struct inode *tip = NULL;
     struct dentry *dp = NULL;
     struct file *filp = NULL;
+#if defined(AFS_CACHE_VNODE_PATH)
+    int code;
+    int dummy;
+    char fname[1024];
+#endif
     AFS_STATCNT(osi_UFSOpen);
     if (cacheDiskType != AFS_FCACHE_TYPE_UFS) {
 	osi_Panic("UFSOpen called for non-UFS cache\n");
