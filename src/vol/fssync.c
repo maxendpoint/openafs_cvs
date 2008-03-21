0d67ffdcf7322665b281f09bb80eee47de042c67
shadow <shadow>
2008-03-21 16:59:26 +0000

    DELTA STABLE14-aix-unix-sockets-fix-200803221 AUTHOR tkeiser@sinenomine.net LICENSE IPL10 FIXES 88088
    use aix's macro for the size of sockaddr_un

--- src/vol/fssync.c
+++ src/vol/fssync.c
@@ -51,7 +51,7 @@ static int newVLDB = 1;
 #include <afs/afsutil.h>
 
 RCSID
-    ("$Header: /cvs/openafs/src/vol/Attic/fssync.c,v 1.26.2.8 2007/11/26 21:21:57 shadow Exp $");
+    ("$Header: /cvs/openafs/src/vol/Attic/fssync.c,v 1.26.2.9 2008/03/21 16:59:26 shadow Exp $");
 
 #include <sys/types.h>
 #include <stdio.h>
@@ -162,6 +162,17 @@ extern int LogLevel;
  */
 struct Lock FSYNC_handler_lock;
 
+
+/*
+ * On AIX, connect() and bind() require use of SUN_LEN() macro;
+ * sizeof(struct sockaddr_un) will not suffice.
+ */
+#if defined(AFS_AIX_ENV) && defined(USE_UNIX_SOCKETS)
+#define AFS_SOCKADDR_LEN(sa)  SUN_LEN(sa)
+#else
+#define AFS_SOCKADDR_LEN(sa)  sizeof(*sa)
+#endif
+
 int
 FSYNC_clientInit(int f)
 {
@@ -177,7 +188,7 @@ FSYNC_clientInit(int f)
 
     for (;;) {
 	FS_sd = getport(&addr);
-	if (connect(FS_sd, (struct sockaddr *)&addr, sizeof(addr)) >= 0)
+	if (connect(FS_sd, (struct sockaddr *)&addr, AFS_SOCKADDR_LEN(&addr)) >= 0)
 	    return 1;
 	if (!f) {
 	    FSYNC_clientFinis();
@@ -367,7 +378,7 @@ FSYNC_sync()
 
     for (numTries = 0; numTries < MAX_BIND_TRIES; numTries++) {
 	if ((code =
-	     bind(AcceptSd, (struct sockaddr *)&addr, sizeof(addr))) == 0)
+	     bind(AcceptSd, (struct sockaddr *)&addr, AFS_SOCKADDR_LEN(&addr))) == 0)
 	    break;
 #ifdef USE_UNIX_SOCKETS
 	code = errno;
