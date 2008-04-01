350ada28c5ad4118b6c55c563b68392e11b567ec
shadow <shadow>
2008-04-01 17:44:40 +0000

    DELTA DEVEL15-aklog-tidyup-20080401 AUTHOR sxw@inf.ed.ac.uk LICENSE IPL10 FIXES 92643
    remove windows code (osolete) from unix branch
    do other minor cleanup

--- src/aklog/aklog.h
+++ src/aklog/aklog.h
@@ -1,5 +1,5 @@
 /* 
- * $Id: aklog.h,v 1.5.2.1 2007/07/11 04:31:06 shadow Exp $
+ * $Id: aklog.h,v 1.5.2.2 2008/04/01 17:44:40 shadow Exp $
  *
  * Copyright 1990,1991 by the Massachusetts Institute of Technology
  * For distribution and copying rights, see the file "mit-copyright.h"
@@ -7,10 +7,6 @@
 
 #ifndef __AKLOG_H__
 #define __AKLOG_H__
-
-#if !defined(lint) && !defined(SABER)
-static char *rcsid_aklog_h = "$Id: aklog.h,v 1.5.2.1 2007/07/11 04:31:06 shadow Exp $";
-#endif /* lint || SABER */
 
 #include <krb5.h>
 #include "linked_list.h"
