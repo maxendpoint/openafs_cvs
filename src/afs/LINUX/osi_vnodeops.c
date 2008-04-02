bea7f244e5bdaee9d80c84016753f37316464375
shadow <shadow>
2008-04-02 13:08:19 +0000

    DELTA linux-flush-unlock-20080402 AUTHOR haba@pdc.kth.se LICENSE IPL10 FIXES
    because i let this slip out like this. sigh.

--- src/afs/LINUX/osi_vnodeops.c
+++ src/afs/LINUX/osi_vnodeops.c
@@ -22,7 +22,7 @@
 #include "afs/param.h"
 
 RCSID
-    ("$Header: /cvs/openafs/src/afs/LINUX/osi_vnodeops.c,v 1.147 2008/03/26 04:16:51 shadow Exp $");
+    ("$Header: /cvs/openafs/src/afs/LINUX/osi_vnodeops.c,v 1.148 2008/04/02 13:08:19 shadow Exp $");
 
 #include "afs/sysincludes.h"
 #include "afsincludes.h"
@@ -588,8 +588,10 @@ afs_linux_flush(struct file *fp)
 
     AFS_GLOCK();
 
-    if (fp->f_flags | O_RDONLY)     /* readers dont flush */
+    if (fp->f_flags | O_RDONLY) {     /* readers dont flush */
+	AFS_GUNLOCK();
 	return 0;
+    }
 
     credp = crref();
     vcp = VTOAFS(FILE_INODE(fp));
