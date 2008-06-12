bfdecc7e28a1c0b6a02e7c80d88a802a928239cb
shadow <shadow>
2008-06-12 17:24:51 +0000

    DELTA DEVEL153X-volume-limit-higher-20080612 AUTHOR shadow@dementia.org LICENSE IPL10 FIXES
    allow more volume lookups to be cached

--- src/afs/afs_init.c
+++ src/afs/afs_init.c
@@ -17,7 +17,7 @@
 #include "afs/param.h"
 
 RCSID
-    ("$Header: /cvs/openafs/src/afs/afs_init.c,v 1.37.4.5.2.1 2008/05/23 14:25:34 shadow Exp $");
+    ("$Header: /cvs/openafs/src/afs/afs_init.c,v 1.37.4.5.2.2 2008/06/12 17:24:51 shadow Exp $");
 
 #include "afs/stds.h"
 #include "afs/sysincludes.h"	/* Standard vendor system headers */
@@ -123,8 +123,8 @@ afs_CacheInit(afs_int32 astatSize, afs_int32 afiles, afs_int32 ablocks,
      */
     if (aVolumes < 50)
 	aVolumes = 50;
-    else if (aVolumes > 3000)
-	aVolumes = 3000;
+    else if (aVolumes > 32767)
+	aVolumes = 32767;
 
     tv = (struct volume *)afs_osi_Alloc(aVolumes * sizeof(struct volume));
     for (i = 0; i < aVolumes - 1; i++)
