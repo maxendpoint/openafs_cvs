1f5de7d26328f8e5f049d946f456a23caa342e4c
shadow <shadow>
2008-08-22 04:31:32 +0000

    DELTA rx-allow-jumbogram-disabling-really-20080822 AUTHOR shadow@dementia.org LICENSE IPL10 FIXES
    right now, jumbograms can still be sent when -nojumbo is specified. make the
    current macro a default and override when we're supposed to.

--- src/rx/rx_kcommon.c
+++ src/rx/rx_kcommon.c
@@ -15,7 +15,7 @@
 #include "afs/param.h"
 
 RCSID
-    ("$Header: /cvs/openafs/src/rx/rx_kcommon.c,v 1.66 2008/04/09 16:39:40 shadow Exp $");
+    ("$Header: /cvs/openafs/src/rx/rx_kcommon.c,v 1.67 2008/08/22 04:31:32 shadow Exp $");
 
 #include "rx/rx_kcommon.h"
 
@@ -475,7 +475,7 @@ rxi_InitPeerParams(register struct rx_peer *pp)
     pp->natMTU = MIN(pp->ifMTU, OLD_MAX_PACKET_SIZE);
     pp->ifDgramPackets =
 	MIN(rxi_nDgramPackets,
-	    rxi_AdjustDgramPackets(RX_MAX_FRAGS, pp->ifMTU));
+	    rxi_AdjustDgramPackets(rxi_nSendFrags, pp->ifMTU));
     pp->maxDgramPackets = 1;
 
     /* Initialize slow start parameters */
