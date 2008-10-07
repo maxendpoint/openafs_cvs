6792749b15110d0baa9a15b8c99c928c9dd01fbc
shadow <shadow>
2008-10-07 17:44:13 +0000

    DELTA DEVEL15-ubik-notruncate-dont-pollute-fd-cache-20081007 AUTHOR shadow@dementia.org LICENSE IPL10 FIXES
    issue suggested by Chaskiel Grundman; while it should not have been an issue
    we now behave usefully in the notruncate recovery code.

--- src/ubik/phys.c
+++ src/ubik/phys.c
@@ -11,7 +11,7 @@
 #include <afs/param.h>
 
 RCSID
-    ("$Header: /cvs/openafs/src/ubik/phys.c,v 1.9.2.5 2008/07/19 06:15:02 rra Exp $");
+    ("$Header: /cvs/openafs/src/ubik/phys.c,v 1.9.2.6 2008/10/07 17:44:13 shadow Exp $");
 
 #include <sys/types.h>
 #ifdef AFS_NT40_ENV
@@ -115,7 +115,7 @@ uphys_open(register struct ubik_dbase *adbase, afs_int32 afid)
     }
     if (bestfd) {		/* found a usable slot */
 	tfd = bestfd;
-	if (tfd->fd >= 0)
+	if (tfd->fd >= 0) 
 	    close(tfd->fd);
 	tfd->fd = fd;
 	tfd->refCount = 1;	/* us */
@@ -137,10 +137,22 @@ uphys_close(register int afd)
 	return EBADF;
     tfd = fdcache;
     for (i = 0; i < MAXFDCACHE; i++, tfd++) {
-	if (tfd->fd == afd && tfd->fileID != -10000) {
-	    tfd->refCount--;
-	    return 0;
-	}
+	if (tfd->fd == afd) 
+	    if (tfd->fileID != -10000) {
+		tfd->refCount--;
+		return 0;
+	    } else {
+		if (tfd->refCount > 0) {
+		    tfd->refCount--;
+		    if (tfd->refCount == 0) {
+			close(tfd->fd);
+			tfd->fd = -1;
+		    }
+		    return 0;
+		}
+		tfd->fd = -1;
+		break;
+	    }
     }
     return close(afd);
 }
@@ -300,8 +312,10 @@ uphys_invalidate(register struct ubik_dbase *adbase, afs_int32 afid)
     for (tfd = fdcache, i = 0; i < MAXFDCACHE; i++, tfd++) {
 	if (afid == tfd->fileID) {
 	    tfd->fileID = -10000;
-	    if (tfd->fd >= 0 && tfd->refCount == 0)
+	    if (tfd->fd >= 0 && tfd->refCount == 0) {
 		close(tfd->fd);
+		tfd->fd = -1;
+	    }
 	    return;
 	}
     }
