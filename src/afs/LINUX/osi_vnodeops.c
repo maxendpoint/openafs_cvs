860d46e0dd25255d8345e1daa8469baef12ab889
shadow <shadow>
2008-10-21 13:44:50 +0000

    DELTA linux24-writepage-antideadlock-20081021 AUTHOR shadow@dementia.org LICENSE IPL10 FIXES
    also make this work for 2.4.x

--- src/afs/LINUX/osi_vnodeops.c
+++ src/afs/LINUX/osi_vnodeops.c
@@ -22,7 +22,7 @@
 #include "afs/param.h"
 
 RCSID
-    ("$Header: /cvs/openafs/src/afs/LINUX/osi_vnodeops.c,v 1.160 2008/10/20 12:09:29 shadow Exp $");
+    ("$Header: /cvs/openafs/src/afs/LINUX/osi_vnodeops.c,v 1.161 2008/10/21 13:44:50 shadow Exp $");
 
 #include "afs/sysincludes.h"
 #include "afsincludes.h"
@@ -1901,10 +1901,15 @@ afs_linux_writepage_sync(struct inode *ip, struct page *pp,
 	maybe_unlock_kernel();
 	crfree(credp);
 	kunmap(pp);
+#ifdef AFS_LINUX26_ENV
 #if defined(WRITEPAGE_ACTIVATE)
 	return WRITEPAGE_ACTIVATE;
 #else
 	return AOP_WRITEPAGE_ACTIVATE;
+#endif
+#else
+	/* should mark it dirty? */
+	return(0); 
 #endif
     }
     ReleaseReadLock(&vcp->lock);
