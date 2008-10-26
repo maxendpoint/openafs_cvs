b5f8e6d916398c58612d2a2bad80255cbe43e635
shadow <shadow>
2008-10-26 00:00:07 +0000

    DELTA DEVEL15-aklog-chatty-20081025 AUTHOR sxw@inf.ed.ac.uk LICENSE IPL10 FIXES
    make aklog not accidentally be chatty

--- src/aklog/aklog_main.c
+++ src/aklog/aklog_main.c
@@ -1,5 +1,5 @@
 /* 
- * $Id: aklog_main.c,v 1.12.2.15 2008/04/01 18:15:40 shadow Exp $
+ * $Id: aklog_main.c,v 1.12.2.16 2008/10/26 00:00:07 shadow Exp $
  *
  * Copyright 1990,1991 by the Massachusetts Institute of Technology
  * For distribution and copying rights, see the file "mit-copyright.h"
@@ -36,7 +36,7 @@
 
 #include <afsconfig.h>
 RCSID
-     ("$Header: /cvs/openafs/src/aklog/aklog_main.c,v 1.12.2.15 2008/04/01 18:15:40 shadow Exp $");
+     ("$Header: /cvs/openafs/src/aklog/aklog_main.c,v 1.12.2.16 2008/10/26 00:00:07 shadow Exp $");
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
@@ -528,8 +528,10 @@ static int auth_to_cell(krb5_context context, char *cell, char *realm)
 				    "%s.\n", progname, cell_to_use);
 			    exit(AKLOG_MISC);
 			}
-			printf("We've deduced that we need to authenticate to"
-			       " realm %s.\n", realm_of_cell);
+			if (dflag) {
+			    printf("We've deduced that we need to authenticate"
+			           " to realm %s.\n", realm_of_cell);
+			}
 		    }
 		    status = get_credv5(context, AFSKEY, cell_to_use, 
 				        realm_of_cell, &v5cred);
