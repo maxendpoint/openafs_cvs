30924c88c63875fccfb7f19ab44fc4a6f074a351
jaltman <jaltman>
2008-11-07 18:19:38 +0000

    DELTA windows-nim-afs-longer-cell-names-20081107 AUTHOR asanka@secure-endpoints.com LICENSE MIT
    permit cell and realm names to be longer than the input field.

--- src/WINNT/netidmgr_plugin/afspext.h
+++ src/WINNT/netidmgr_plugin/afspext.h
@@ -22,7 +22,7 @@
  * SOFTWARE.
  */
 
-/* $Id: afspext.h,v 1.2 2006/10/16 13:54:32 jaltman Exp $ */
+/* $Id: afspext.h,v 1.3 2008/11/07 18:19:38 jaltman Exp $ */
 
 #ifndef __AFSPLUGIN_EXT_H
 #define __AFSPLUGIN_EXT_H
@@ -40,8 +40,8 @@
 
   @{*/
 
-#define MAXCELLCHARS   64
-#define MAXHOSTCHARS   64
+#define MAXCELLCHARS   256
+#define MAXHOSTCHARS   256
 #define MAXHOSTSPERCELL 8
 
 #define TRANSARCAFSDAEMON "TransarcAFSDaemon"
