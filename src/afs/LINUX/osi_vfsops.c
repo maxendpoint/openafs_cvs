d7d77af22c0da0c4a158a418c48418fa4177150c
shadow <shadow>
2009-01-09 15:09:14 +0000

    DELTA linux-truncate-race-20090109 AUTHOR sxw@inf.ed.ac.uk LICENSE IPL10 FIXES 124094
    avoid a race during truncation which trips an issue when fsx is run

--- src/afs/LINUX/osi_vfsops.c
+++ src/afs/LINUX/osi_vfsops.c
@@ -16,7 +16,7 @@
 #include "afs/param.h"
 
 RCSID
-    ("$Header: /cvs/openafs/src/afs/LINUX/osi_vfsops.c,v 1.62 2009/01/09 14:57:22 shadow Exp $");
+    ("$Header: /cvs/openafs/src/afs/LINUX/osi_vfsops.c,v 1.63 2009/01/09 15:09:14 shadow Exp $");
 
 #define __NO_VERSION__		/* don't define kernel_version in module.h */
 #include <linux/module.h> /* early to avoid printf->printk mapping */
@@ -544,7 +544,7 @@ vattr2inode(struct inode *ip, struct vattr *vp)
     ip->i_mode = vp->va_mode;
     ip->i_uid = vp->va_uid;
     ip->i_gid = vp->va_gid;
-    i_size_write(i_size, vp->va_size);
+    i_size_write(ip, vp->va_size);
 #if defined(AFS_LINUX26_ENV)
     ip->i_atime.tv_sec = vp->va_atime.tv_sec;
     ip->i_atime.tv_nsec = 0;
